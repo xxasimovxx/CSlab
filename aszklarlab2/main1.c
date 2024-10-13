f0a17380996b6ce9f60ac64e0d444e9d0a79e2f3
xxasimovxx <asimovpl@wp.pl>
2024-10-13 21:12:20 +0200

    lul

--- aszklarlab2/main1.c
+++ aszklarlab2/main1.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void printArray(double arr[], int N);
 void readArray(double arr[], int N);
+void getArrays(double Z[], int size);
 
 int main (){
+  int size;
 
-  double arr[] = {1.0,2.0,3.0};
+  printf("Give size of array [0-32]: ");
+  scanf("%d", &size);
+  getchar();
+
+  if(size > 32){
+
+    puts("Wrong value");
+    return -1;
+  }
+
+  double Z[size];
+
+  getArrays(Z, size);
   
-  printArray(arr, 2);
-
+  printArray(Z, size);
 
   return 0;
 
@@ -35,6 +50,25 @@ void readArray(double arr[], int N){
     printArray(A, N);
 
   }
+}
+
+void getArrays(double Z[], int size){
+  char * piece;
+  char * endptr;
+  char input[100];
+    
+  printf("Input %d values: ", size);
+  fgets(input, sizeof(input), stdin);
+  input[strcspn(input, "\n")] = '\0';                             
+  printf("%s", input);
+  piece = strtok(input, " ");           
+  Z[0] = strtod(piece, &endptr);
+
+  for(int i = 1; i < size; i++){
+    piece = strtok(NULL, " ");           
+    Z[i] = strtod(piece, &endptr);
+    
+  } 
 }
 
 
